bd10c880b467541590d41b19c4d2640204307573
Stephen Duncanson <Stephen.duncanson@gmail.com>
2020-10-15 13:52:42 -0400

    added Poisson approximation

--- binomial_stats.rkt
+++ binomial_stats.rkt
@@ -15,3 +15,18 @@
    (* (n-choose-k n x) (expt p x) (expt q (- n x)))))
 
 (binomial 5 250 0.02)
+
+(define (poisson x n p)
+  (define (factorial n)
+    (define (iter product counter)
+      (if (> counter n)
+          product
+          (iter (* counter product)
+                (+ counter 1))))
+    (iter 1 1))
+  (let* ((e 2.7182818)
+         (lam (* n p)))
+    (* (expt e (- lam)) (/ (expt lam x) (factorial x)))))
+
+(poisson 5 250 0.02)
+
