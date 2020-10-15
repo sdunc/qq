a3bb417919c35f5d9e0442e33d97de88c8327e90
Stephen Duncanson <Stephen.duncanson@gmail.com>
2020-10-15 11:34:58 -0400

    Add files via upload

---
+++ req.rkt
@@ -0,0 +1,24 @@
+; Stephen Duncanson
+; Find Req for series or parallel resistors
+
+; a list of resistors having values of 2,3, and 4 ohms.
+(define res-list (list 2 2 2))
+
+; series resisors are just added up
+(define (sum-list list)
+  (if (= (length list) 1)
+      (car list)
+      (+ (car list) (sum-list (cdr list)))))
+
+(define (get-series-req rlist)
+  ; move sum-list in here? to be a helper?
+  ; returns the equiv. resistance of a list of resistors in series.
+  (sum-list rlist))
+
+(define (get-parallel-req rlist)
+  ; returns req of list of parallel resistors.
+  (let ((irlist (map (lambda (x) (/ 1 x)) rlist)))
+    (/ 1 (sum-list irlist))))
+
+(get-series-req res-list)
+(get-parallel-req res-list)
